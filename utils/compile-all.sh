082332a4a07665f8d6b6eac2326d45cddff8241f
Sam <barrett370@gmail.com>
2020-01-06 17:50:13 +0000

    tools working

--- utils/compile-all.sh
+++ utils/compile-all.sh
@@ -1,15 +1,14 @@
 sd=$(pwd)
-tsc "$sd"/utils/converter.ts
-echo $sd
-cd Y3 || exit
+tsc "$sd"/converter.ts
+cd ../public/content/modules || exit
 
 for D in $(find . -type d); do
   if [[ $D =~ '/source$' ]]; then
     for F in $(find $D -type f); do
       echo "compiling $F"
-      node "$sd"/utils/converter.js -f "$F"
+      node "$sd"/converter.js -f "$F"
     done
-    mv "$D"/*.html "$D"/../out/
+    mv "$D"/*.html "$D"/../lectures/
   fi
 
 done
--- utils/diff-compile-all.sh
+++ utils/diff-compile-all.sh
@@ -1,16 +1,15 @@
 sd=$(pwd)
 tsc "$sd"/converter.ts
-echo $sd
 cd ../public/content/modules || exit
 CHANGED_MD=$(git diff-index --name-only HEAD -- | grep '.md')
 #echo $CHANGED_MD
 for D in $(find . -type d); do
   if [[ $D =~ '/source$' ]]; then
-    echo "Looking in $D"
     for F in $(find $D -type f); do
       for item in "${CHANGED_MD[@]}"; do
         str1="${F:2}"
-        str2="${item}"
+        str2="${item#public/content/modules/}"
+        echo $str1 $str2
         [ "$str1" = "$str2" ] && node "$sd"/converter.js -f "$F"
       done
     done
